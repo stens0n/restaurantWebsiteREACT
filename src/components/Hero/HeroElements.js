d8951a9e1a85ea796c26e232adee3a50cbefaf0d
stens0n <astenson010@gmail.com>
2022-06-07 12:05:08 -0400

    Hero

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -10,3 +10,29 @@ export const HeroContainer = styled.div`
     background-position: center;
     background-size: cover;
 `;
+
+export const HeroContent = styled.div`
+    height: calc(100vh, -80px);
+    max-height: 100%;
+    width: 100vw;
+    padding: 0rem calc((100vq - 1300px) /2 );
+`;
+
+export const HeroItems = styled.div`
+    @media and (max-width: 650px) {
+        width: 100%;
+    };
+    font-weight: bold;
+    line-height: 1;
+    text-transform: uppercase;
+    color: #fff;
+    width: 650px;
+    padding: 0 2rem;
+    max-height: 100%;
+    height: 100vh;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    display: flex;
+`;
+
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import {HeroContainer} from './HeroElements';
+import {HeroContainer,
+        HeroContent,
+        HeroItems,
+        HeroH1,
+        HeroP,
+        HeroBtn 
+        } from './HeroElements';
 
 const Hero = () => {
   return (
     <HeroContainer>
       <Navbar/>
+      <HeroContent> 
+        <HeroItems> 
+          <HeroH1>Greatest Food Ever</HeroH1>
+          <HeroP>Ready in minutes!</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
 
     </HeroContainer>
   )
